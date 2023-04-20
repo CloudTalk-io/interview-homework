3be6c8b8db2f4e332ec6056e70e194e301e43b2c
Krzysztof Cywiński <krzysztofcyw@gmail.com>
2023-04-20 10:19:11 +0200

    XYZ-1 Fix existing tests
    Changes made:
    - added mocket input to the component under test
    - removed test that seems to be a leftover

--- src/app/pages/items-list/list-item/list-item.component.spec.ts
+++ src/app/pages/items-list/list-item/list-item.component.spec.ts
@@ -1,10 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListItemComponent } from './list-item.component';
+import { WarehouseItem } from "../../../core/models/warehouseItem";
 
 describe('ListItemComponent', () => {
   let component: ListItemComponent;
   let fixture: ComponentFixture<ListItemComponent>;
+
+  const mockedInput: WarehouseItem = {
+    imageUrl: `imageUrl`,
+    id: 1,
+    name: `name`,
+    description: `description`,
+    quantity: 2,
+    unitPrice: 3,
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,6 +24,7 @@ describe('ListItemComponent', () => {
 
     fixture = TestBed.createComponent(ListItemComponent);
     component = fixture.componentInstance;
+    component.item = mockedInput;
     fixture.detectChanges();
   });
 
