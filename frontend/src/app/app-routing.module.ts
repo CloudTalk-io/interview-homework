470421ff48521cc78c6537dc0281d106a3d0c6ff
Anton Shyrokoborodov <anton.shyrokoborodov@gmail.com>
2024-07-18 15:51:05 +0200

    chore: add wildcard route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {ItemsListComponent} from "./pages/items-list/items-list.component";
+import {ItemsListComponent} from './pages/items-list/items-list.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ItemsListComponent
-  }
+  },
+  { path: '**',   redirectTo: '/', pathMatch: 'full' }
 ];
 
 @NgModule({
