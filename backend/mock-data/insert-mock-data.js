e602c49ccaa60a727d73f02fdb3cab709edab059
Anton Shyrokoborodov <anton.shyrokoborodov@gmail.com>
2024-07-26 12:27:47 +0200

    chore: enhance mock data generation

--- backend/mock-data/insert-mock-data.js
+++ backend/mock-data/insert-mock-data.js
@@ -4,6 +4,10 @@ const mongodb = require('../mongodb');
 const insertProducts = require('./products');
 
 (async () => {
+  if (process.env.NODE_ENV === 'production') {
+    return;
+  }
+
   await mongodb.connect();
 
   await Promise.all([insertProducts()]);
--- backend/mock-data/products.js
+++ backend/mock-data/products.js
@@ -1,3 +1,4 @@
+const { faker } = require('@faker-js/faker');
 const Product = require('../models/product.model');
 const logger = require('../utils/logger')('mock-data:products');
 
@@ -5,13 +6,18 @@ module.exports = function () {
   logger.debug('Insert products mock data');
 
   return Product.insertMany(
-    Array(10).fill({
-      imageUrl: '/assets/logo_black.svg',
-      name: 'CloudTalk logo sticker',
-      description:
-        'High-quality sticker of the best cloud calling solution provider in  the world',
-      quantity: 100,
-      unitPrice: 10,
-    })
+    Array(10)
+      .fill(null)
+      .map(() => ({
+        imageUrl: faker.image.urlLoremFlickr({
+          category: 'cats',
+          width: 50,
+          height: 50,
+        }),
+        name: `${faker.commerce.productAdjective()} ${faker.commerce.product()}`,
+        description: faker.commerce.productDescription(),
+        quantity: faker.number.int({ min: 1, max: 100 }),
+        unitPrice: faker.commerce.price({ min: 10, max: 100 }),
+      }))
   );
 };
